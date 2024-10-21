d5dca9d4bd4bbc4070bd3ce5e856d6fe3b825034
unknown <thomas.chaures@davinci.edu.ar>
2024-10-21 00:12:42 -0300

    cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import autosRutas from './routes/peugot.routes.js'
 import apiAutosRutas from './api/routes/peugot-api.routes.js'
-
+import cors from 'cors' 
 
 const app = express()
 app.use( express.static("img"))
 app.use( express.urlencoded({ extended: true }) )
 app.use(express.json())
+app.use(cors())
+
 app.use(autosRutas)
 app.use('/api',apiAutosRutas)
 
